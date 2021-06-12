edbc3648e34aaa45a704672593c2e9182da0828c
Daria Pavlova <56891506+jswark@users.noreply.github.com>
2021-06-12 21:53:42 +0300

    db system (#3)
    * ui
    
    * workin' w/ db + small fixes in main functions
    
    * create and add
    
    Co-authored-by: m_khorosh <marina.khoroshilova@gmail.com>

--- sql2.py
+++ sql2.py
@@ -1,42 +1,121 @@
-
+from sqlalchemy import Table, Column, Integer, Float, String, MetaData, ForeignKey, text, create_engine, insert
 from sqlalchemy.orm import mapper, relation
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, text, create_engine
+from sqlalchemy_utils import database_exists, create_database
+
+
+from sqlalchemy import MetaData
+
+# create db
+def create_db():
+    global engine
+    engine = create_engine('postgresql://postgres:12345@localhost/postgres', echo=True)
+
+    if not database_exists(engine.url):
+        create_database(engine.url)
+
+    global metadata
+    metadata = MetaData(engine)
+
+    return engine
+
+def add_Table():
+    class PokedexClass:
+        def __init__(self, id, name_rus, name_eng, elem, type, height, weight_t):
+            self.id = id
+            self.name_rus=name_rus
+            self.name_eng = name_eng
+            self.element = elem
+            self.type= type
+            self.height = height
+            self.weight_type = weight_t
+
+        def __repr__(self):
+            return self.id
+
+    class HealthClass:
+        def __init__(self, weight, health):
+            self.weight = weight
+            self.health = health
+
+        def __repr__(self):
+            return self.weight
+
+    class ElementClass:
+        def __init__(self, element, attack, total):
+            self.element = element
+            self.attack = attack
+            self.total=total
+
+        def __repr__(self):
+            return self.element
+
+    pokedex_table = Table('pokedex', metadata,
+                          Column('id', Integer, primary_key=True),
+                          Column('name_rus', String, nullable=False, index=True),
+                          Column('name_eng', String, nullable=False),
+                          Column('element', ForeignKey('elements.element'), nullable=False),
+                          Column('type', String, nullable=False),
+                          Column('height', Float, nullable=False),
+                          Column('weight_type', ForeignKey('healths.weight'), nullable=False))
+
+    health_table = Table('healths', metadata,
+                         Column('weight', Integer, primary_key=True),
+                         Column('health', Integer, nullable=False))
+
+    elements_table = Table('elements', metadata,
+                           Column('element', String, primary_key=True),
+                           Column('attack', Integer, nullable=False),
+                           Column('total', Integer, nullable=False))
+
+    metadata.create_all(engine)  # creates the tables
+
+    mapper(PokedexClass, pokedex_table )
+    mapper(HealthClass, health_table, properties = {'healths': relation(PokedexClass, backref='weight_t')})
+    mapper(ElementClass, elements_table, properties = {'elements': relation(PokedexClass, backref='elem')})
+
+    from sqlalchemy.orm import sessionmaker
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    # data
+    pokemons = [[1, 'Бульбазавр', 'Bulbasaur', 'травяной', 'зерно', 0.7, 1],
+                [2, 'Чармандер', 'Charmander', 'огненный', 'ящерица', 0.6, 3],
+                [3, 'Сквиртл', 'Squirtle', 'водный', 'черепаха', 0.5, 3]]
+
+    health = [[1, 500],
+              [2, 750],
+              [3, 1000]]
+
+    elements = [['травяной', 77],
+                ['огненный', 69],
+                ['водный', 80],
+                ['электрический', 71],
+                ['обычный', 60]]
+
+    count = {'травяной': 0, 'огненный': 0, 'водный': 0, 'электрический': 0, 'обычный': 0}
+
+    for i in pokemons:
+        count[i[3]]+=1
+
+    for i in health:
+        exists = session.query(HealthClass).filter_by(weight = i[0]).first()
+        if not exists:
+            pokem = HealthClass(i[0], i[1])
+            session.add(pokem)
+
+    for i in elements:
+        exists = session.query(ElementClass).filter_by(element=i[0]).first()
+        if not exists:
+            pokem = ElementClass(i[0], i[1], count[i[0]])
+            session.add(pokem)
+
+    for i in pokemons:
+        exists = session.query(PokedexClass).filter_by(id = i[0]).first()
+        if not exists:
+            pokem = PokedexClass(i[0], i[1], i[2], i[3], i[4], i[5], i[6])
+            session.add(pokem)
 
 
 
-engine = create_engine('postgresql://jswark:12345@localhost/pockemons', echo=True)
-metadata = MetaData(engine)
-books_table = Table('books', metadata, autoload=True) # create a Table object
-authors_table = Table('authors', metadata, autoload=True) # create a Table object
+    session.commit()
 
 
-class Author:
-    def __init__(self, name):
-        self.name = name
-    def __repr__(self):
-        return self.name
-
-
-class Book:
-    def __init__(self, title, description, author):
-        self.title = title
-        self.description = description
-        self.author = author
-
-    def __repr__(self):
-        return self.title
-
-
-mapper(Book, books_table)
-mapper(Author, authors_table, properties = {
-'books': relation(Book, backref='author')})
-
-
-from sqlalchemy.orm import sessionmaker
-Session = sessionmaker(bind=engine)
-session = Session()
-auth = Author('Aleksandr Semin 3')
-auth2 = Author('Aleksandr Semin 4')
-session.add(auth)
-session.add(auth2)
-session.commit()
--- tkinterapp.py
+++ tkinterapp.py
@@ -1,134 +1,92 @@
 from tkinter import *
 from tkinter import ttk
 
-from sqlalchemy import create_engine
-
-from sqlalchemy import MetaData, Table
+from sqlalchemy import MetaData
 from sqlalchemy import text
+from sqlalchemy_utils import drop_database
+
+import sql2
 import pymysql
-
-pymysql.install_as_MySQLdb()
-
-engine = create_engine('postgresql://jswark:12345@localhost/pockemons', echo=True)
-conn = engine.connect()
-meta = MetaData(bind=conn)
-meta.reflect()
-
-
-################################################################################################
-################################################################################################
-################################################################################################
 
 def delete_row():
     selectedItem = tab.focus()
     selectedItem = list(tab.item(selectedItem)['values'])
 
-    tablica = meta.tables[odabir.get()]
+    table = meta.tables[choose.get()]
 
-    stupci = list(tablica.columns.keys())
+    columns = list(table.columns.keys())
     delete_string = ""
 
-    for i in range(0, len(stupci)):
-        if (stupci[i] == "jmbag" or stupci[i] == "jmbagStudent"):
-            selectedItem[i] = str(selectedItem[i]).zfill(10)
-        elif (stupci[i] == "jmbg" or stupci[i] == "jmbgNastavnik"):
-            selectedItem[i] = str(selectedItem[i]).zfill(13)
-        elif (stupci[i] == "oib" or stupci[i] == "oibUstanova"):
-            selectedItem[i] = str(selectedItem[i]).zfill(11)
-        else:
-            delete_string += stupci[i] + "='" + str(selectedItem[i]) + "' AND "
+    for i in range(0, len(columns)):
+        delete_string += columns[i] + "='" + str(selectedItem[i]) + "' AND "
 
-    # Brise zadnji AND
     delete_string = delete_string[0:len(delete_string) - 4]
     print(delete_string)
 
-    query = tablica.delete().where(text(delete_string))
+    query = table.delete().where(text(delete_string))
     result = conn.execute(query)
 
     on_select()
 
+def clear_db():
+    table = meta.tables[choose.get()]
+    query = table.delete()
+    result = conn.execute(query)
+    on_select()
 
 # --------------------------------------------------------------------
 def add_row():
-    #####################################
     def add_query():
         add_list = []
 
         keys = entries.keys()
-        for stupac in keys:
-            if (stupac == "jmbag" or stupac == "jmbagStudent"):
-                add_list.append(str(entries[stupac].get()).zfill(10))
-            elif (stupac == "jmbg" or stupac == "jmbgNastavnik"):
-                add_list.append(str(entries[stupac].get()).zfill(13))
-            elif (stupac == "oib" or stupac == "oibUstanova"):
-                add_list.append(str(entries[stupac].get()).zfill(11))
-            else:
-                add_list.append(str(entries[stupac].get()))
+        for col in keys:
+            add_list.append(str(entries[col].get()))
 
-        query = tablica.insert().values(tuple(add_list))
+        query = table.insert().values(tuple(add_list))
         result = conn.execute(query)
 
         on_select()
         add.destroy()
 
-    #####################################
-
     add = Tk()
     add.title("Add")
-    tablica = meta.tables[odabir.get()]
+    table = meta.tables[choose.get()]
 
     entries = {}
-    stupciTablice = tablica.columns.keys()
+    table_cols = table.columns.keys()
 
     red = 0
-    for stupac in stupciTablice:
-        label = Label(add, text=stupac, width=20)
+    for col in table_cols:
+        label = Label(add, text=col, width=20)
         label.grid(row=red, column=0)
 
-        entries[stupac] = Entry(add, width=30)
-        entries[stupac].grid(row=red, column=1)
+        entries[col] = Entry(add, width=30)
+        entries[col].grid(row=red, column=1)
 
         red += 1
 
     add_b = Button(add, text="Add", command=add_query, width=10)
     add_b.grid(row=red, column=0, columnspan=2, pady=5)
 
-
 # --------------------------------------------------------------------
 def edit_row():
-    #####################################
     def edit_query():
 
         edit_values = []
         edit_where = ""
 
-        for i in range(0, len(stupci)):
-
-            if (stupci[i] == "jmbag" or stupci[i] == "jmbagStudent"):
-                edit_values.append(str(entries[stupci[i]].get()).zfill(10))
-                edit_where += stupci[i] + "='" + str(selectedItem[i]).zfill(10) + "' AND "
-
-            elif (stupci[i] == "jmbg" or stupci[i] == "jmbgNastavnik"):
-                edit_values.append(str(entries[stupci[i]].get()).zfill(13))
-                edit_where += stupci[i] + "='" + str(selectedItem[i]).zfill(13) + "' AND "
-
-            elif (stupci[i] == "oib" or stupci[i] == "oibUstanova"):
-                edit_values.append(str(entries[stupci[i]].get()).zfill(11))
-                edit_where += stupci[i] + "='" + str(selectedItem[i]).zfill(11) + "' AND "
-
-            else:
-                edit_values.append(str(entries[stupci[i]].get()))
-                edit_where += stupci[i] + "='" + str(selectedItem[i]) + "' AND "
+        for i in range(0, len(col)):
+            edit_values.append(str(entries[col[i]].get()))
+            edit_where += col[i] + "='" + str(selectedItem[i]) + "' AND "
 
         edit_where = edit_where[0:len(edit_where) - 4]
 
-        query = tablica.update().where(text(edit_where)).values(tuple(edit_values))
+        query = table.update().where(text(edit_where)).values(tuple(edit_values))
         result = conn.execute(query)
 
         on_select()
         edit.destroy()
-
-    #####################################
 
     edit = Tk()
     edit.title("Edit")
@@ -136,70 +94,125 @@ def edit_row():
     selectedItem = tab.focus()
     selectedItem = list(tab.item(selectedItem)['values'])
 
-    tablica = meta.tables[odabir.get()]
-    stupci = list(tablica.columns.keys())
-    delete_string = ""
+    table = meta.tables[choose.get()]
+    col = list(table.columns.keys())
 
     entries = {}
 
-    for i in range(0, len(stupci)):
-        if (stupci[i] == "jmbag" or stupci[i] == "jmbagStudent"):
-            selectedItem[i] = str(selectedItem[i]).zfill(10)
-        elif (stupci[i] == "jmbg" or stupci[i] == "jmbgNastavnik"):
-            selectedItem[i] = str(selectedItem[i]).zfill(13)
-        elif (stupci[i] == "oib" or stupci[i] == "oibUstanova"):
-            selectedItem[i] = str(selectedItem[i]).zfill(11)
-
-        label = Label(edit, text=stupci[i], width=20)
+    for i in range(0, len(col)):
+        label = Label(edit, text=col[i], width=20)
         label.grid(row=i, column=0)
 
-        entries[stupci[i]] = Entry(edit, width=30)
-        entries[stupci[i]].insert(0, str(selectedItem[i]))
-        entries[stupci[i]].grid(row=i, column=1)
+        entries[col[i]] = Entry(edit, width=30)
+        entries[col[i]].insert(0, str(selectedItem[i]))
+        entries[col[i]].grid(row=i, column=1)
 
     edit_b = Button(edit, text="Save Editing", command=edit_query, width=10)
-    edit_b.grid(row=len(stupci), column=0, columnspan=2, pady=5)
+    edit_b.grid(row=len(col), column=0, columnspan=2, pady=5)
 
+def del_button():
+    re_button.destroy()
+    on_select()
 
-# --------------------------------------------------------------------
+def delete_db():
+    for tbl in reversed(meta.sorted_tables):
+        engine.execute(tbl.delete())
+    drop_database(engine.url)
+
+def find_in():
+    def show_find_query():
+        query = f'''
+        select *
+        from {tabs}
+        where {entries[0].get()}='{entries[1].get()}'
+        '''
+
+        result = conn.execute(query)
+        iid_counter = 0
+        for item in tab.get_children():
+            tab.delete(item)
+
+        for row in result:
+            row = list(row)
+            tab.insert(parent='', index='end', text="", iid=iid_counter, values=row)
+            iid_counter += 1
+
+        global re_button
+        re_button = Button(option_frame, text="Return", command=del_button, width=10)
+        re_button.pack(side=LEFT)
+
+        find.destroy()
+
+    find = Tk()
+    find.title("Find")
+
+    selectedItem = tab.focus()
+    selectedItem = list(tab.item(selectedItem)['values'])
+
+    tabs = meta.tables[choose.get()]
+    entries = {}
+
+    label = Label(find, text='field name', width=20)
+    label.grid(row=0, column=0)
+    entries[0] = Entry(find, width=30)
+    entries[0].grid(row=0, column=1)
+
+    label = Label(find, text='find', width=20)
+    label.grid(row=1, column=0)
+    entries[1] = Entry(find, width=30)
+    entries[1].grid(row=1, column=1)
+
+    edit_b = Button(find, text="Show", command=show_find_query, width=10)
+    edit_b.grid(row=2, column=0, columnspan=2, pady=5)
+
 def on_select(event=None):
     for item in tab.get_children():
         tab.delete(item)
 
-    tablica = meta.tables[odabir.get()]
+    table = meta.tables[choose.get()]
 
     tab.heading("#0", text="")
     tab.column("#0", anchor=W, width=0)
 
-    tab['columns'] = list(tablica.columns.keys())
+    tab['columns'] = list(table.columns.keys())
 
-    for stupac in list(tablica.columns.keys()):
-        tab.column(stupac)
-    for stupac in list(tablica.columns.keys()):
-        tab.heading(stupac, text=stupac, anchor=W)
+    for col in list(table.columns.keys()):
+        tab.column(col)
+    for col in list(table.columns.keys()):
+        tab.heading(col, text=col, anchor=W)
 
     iid_counter = 0
-    result = conn.execute(tablica.select())
+    result = conn.execute(table.select())
     for row in result:
         row = list(row)
         tab.insert(parent='', index='end', text="", iid=iid_counter, values=row)
         iid_counter += 1
 
 
-# ----------------------------------------------------------------------------------
+## create db
+engine = sql2.create_db()
+sql2.add_Table()
 
+pymysql.install_as_MySQLdb()
+
+conn = engine.connect()
+meta = MetaData(bind=conn)
+meta.reflect()
+
+## ui config
 window = Tk()
 window.title("Pockemons")
 window.geometry("1300x500")
 
-odabir = StringVar()
-odabir.set(list(meta.tables.keys())[0])
+choose = StringVar()
+choose.set(list(meta.tables.keys())[0])
 
 option_frame = Frame(window)
 
-option = OptionMenu(option_frame, odabir, *list(meta.tables.keys()), command=on_select)
+option = OptionMenu(option_frame, choose, *list(meta.tables.keys()), command=on_select)
 option.pack(side=LEFT)
 
+## main buttons
 add_button = Button(option_frame, text="Add", command=add_row, width=10)
 add_button.pack(side=LEFT)
 
@@ -209,9 +222,15 @@ edit_button.pack(side=LEFT)
 delete_button = Button(option_frame, text="Delete", command=delete_row, width=10)
 delete_button.pack(side=LEFT)
 
+add_button = Button(option_frame, text="Clear all", command=clear_db, width=10)
+add_button.pack(side=LEFT)
+
+add_button = Button(option_frame, text="Find in", command=find_in, width=10)
+add_button.pack(side=LEFT)
+
 option_frame.pack(side=TOP)
 
-# ----- Frame za tablicu -----
+## frame
 tree_frame = Frame(window)
 
 tab = ttk.Treeview(tree_frame)
@@ -228,6 +247,9 @@ tab.pack(fill=BOTH, expand=1)
 
 tree_frame.pack(side=TOP, fill=BOTH, expand=1)
 
+## app
 on_select()
-
 window.mainloop()
+
+## delete db
+delete_db()
